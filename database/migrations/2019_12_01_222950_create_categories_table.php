f5977e75eb8ab0d686b970fc8da1f01567edf069
Abdellah Chadidi <chadidi97@gmail.com>
2019-12-01 23:35:27 +0100

    Add name and slug to categories migration

--- database/migrations/2019_12_01_222950_create_categories_table.php
+++ database/migrations/2019_12_01_222950_create_categories_table.php
@@ -15,6 +15,8 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name', 100);
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
