ee9dceede4292cf3027e923787d555d9915d2685
Abdellah Chadidi <chadidi97@gmail.com>
2019-12-01 23:50:15 +0100

    Create 15 categories in seed

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Category;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 
 class CategoriesTableSeeder extends Seeder
@@ -11,6 +13,14 @@ class CategoriesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker = \Faker\Factory::create();
+
+        for ($i = 0; $i <= 14; $i++) {
+            $name = $faker->sentence(3);
+            Category::create([
+                'name' => $name,
+                'slug' => Str::slug($name)
+            ]);
+        }
     }
 }
