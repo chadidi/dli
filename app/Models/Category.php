9bb52202982171d8605af60501a83f9424da56e4
Abdellah Chadidi <chadidi97@gmail.com>
2019-12-02 00:42:49 +0100

    customise category model route key name

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -15,4 +15,14 @@ class Category extends Model
         'name',
         'slug',
     ];
+
+    /**
+ * Get the route key for the model.
+ *
+ * @return string
+ */
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 }
