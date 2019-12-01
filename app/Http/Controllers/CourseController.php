e438ba82f09a2b4060445415f8dc5028eaf7ac4e
Abdellah Chadidi <chadidi97@gmail.com>
2019-12-02 00:49:45 +0100

    remove unused methods

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -18,16 +18,6 @@ class CourseController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -45,17 +35,6 @@ class CourseController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show(Course $course)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Course  $course
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Course $course)
     {
         //
     }
